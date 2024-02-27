07f0091d9ab1b153606089a444976f2e58facdbd
Bartłomiej Różańśki <rozanski.bartlomiej@gmail.com>
2024-02-27 06:16:52 +0100

    testing CalculateService.calculate_price

--- PricePayload.py
+++ PricePayload.py
@@ -1,11 +1,24 @@
 from typing import Final
 
 class PricePayload:
-    def __init__(self, PRODUCT_PRICE, VAT, AMZ_FEE, FIXED_COSTS, MARGIN, EXTRA_PERC_FROM_SELLING_PRICE, NET_DELIVERY_COST) -> None:
-        self.PRODUCT_PRICE: Final = PRODUCT_PRICE
-        self.VAT: Final = VAT
-        self.AMZ_FEE: Final = AMZ_FEE
-        self.FIXED_COSTS: Final = FIXED_COSTS
-        self.MARGIN: Final = MARGIN
-        self.EXTRA_PERC_FROM_SELLING_PRICE: Final = EXTRA_PERC_FROM_SELLING_PRICE
-        self.NET_DELIVERY_COST: Final = NET_DELIVERY_COST
+    """
+    This class represents product price and contains all elements that can be included in the price.
+
+    Attributes:
+        product_price (int): Net product price.
+        vat (int): Value of VAT for a given product in a given country.
+        amz_fee (int): Value of Amazon fee depending on product category and country.
+        fixed_costs (int): Net fixed costs that the seller needs to cover.
+        margin (int): Margin that will be calculated based on the net product price.
+        extra_perc_from_selling_price (int): Extra percentage added to the selling price if needed.
+        net_delivery_cost (int): Cost of delivering the product to the customer if hidden in price.
+    """
+    def __init__(self, product_price: int, vat: int, amz_fee: int, fixed_costs: int, margin: int, 
+                 extra_perc_from_selling_price: int, net_delivery_cost: int) -> None:
+        self.product_price: Final = product_price
+        self.vat: Final = vat
+        self.amz_fee: Final = amz_fee
+        self.fixed_costs: Final = fixed_costs
+        self.margin: Final = margin
+        self.extra_perc_from_selling_price: Final = extra_perc_from_selling_price
+        self.net_delivery_cost: Final = net_delivery_cost
--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import CalculatorService
 import PricePayload
 
-product_1 = PricePayload()
+
+product_1 = PricePayload.PricePayload(product_price=100, vat=0.23, amz_fee=0.15, fixed_costs=10, margin=0.15, 
+                             extra_perc_from_selling_price=0.2, net_delivery_cost=7)
+calculator = CalculatorService.PriceCalculator
+
+# Dostęp do atrybutów obiektu
+print(vars(product_1))  
+
+# amz_price_product_1 = calculator.calculate_price(product_price=product_1.product_price, )
+
