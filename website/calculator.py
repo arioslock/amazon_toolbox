cad9c05829f97b49a80020ccfbcba5e87bd29bcb
Bartłomiej Różańśki <rozanski.bartlomiej@gmail.com>
2024-03-16 08:57:05 +0100

    cleaned branch for merge with main

--- website/calculator.py
+++ website/calculator.py
@@ -8,15 +8,6 @@ def calculator():
     form = request.form
     dict_from_form = form.to_dict(flat=False) 
     json_data = json.dumps(dict_from_form)
-    print(json_data)
-    # if request.method == 'POST':
-    #     net_product_price = request.form.get('net_product_price')
-    #     vat = request.form.get('vat')
-    #     amazon_fee = request.form.get('amazon_fee')
-    #     fixed_costs = request.form.get('fixed_costs')
-    #     margin = request.form.get('margin')
-    #     extra_perc_from_selling_price = request.form.get('extra_perc_from_selling_price')
-    #     net_delivery_cost = request.form.get('net_delivery_cost')
 
     return render_template("calculator.html")
 
