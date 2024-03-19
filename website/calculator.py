144290b4189c690e81f329b60d8591a516d1d1d3
Bartłomiej Różańśki <rozanski.bartlomiej@gmail.com>
2024-03-19 05:58:32 +0100

    Added Controller class Implemented function to calculate price for the web app

--- website/calculator.py
+++ website/calculator.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template, request
 import json
+import Controller
 
 calculator_bp = Blueprint('calculator', __name__)
 
 @calculator_bp.route('/calculator', methods=['GET', 'POST'])
 def calculator():
-    form = request.form
-    dict_from_form = form.to_dict(flat=False) 
-    json_data = json.dumps(dict_from_form)
-    print(json_data)
-    # if request.method == 'POST':
+    if request.method == 'POST':
+        form = request.form
+        dict_from_form = form.to_dict(flat=False) 
+        json_data = json.dumps(dict_from_form)
+        product = Controller.calculate_price_for_web(json_data)
+        print(vars(product))
+        # if request.method == 'POST':
     #     net_product_price = request.form.get('net_product_price')
     #     vat = request.form.get('vat')
     #     amazon_fee = request.form.get('amazon_fee')
