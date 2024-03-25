f86fd15f8f089c93e9c8883e36582ec1eb612c21
Bartłomiej Różańśki <rozanski.bartlomiej@gmail.com>
2024-03-25 05:52:35 +0100

    web app finished

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,8 @@ def creat_app():
     app.config['SECRET_KEY'] = 'amz_toolbox_sc_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+
 
     from .views import views
     from .auth import auth
@@ -23,7 +26,16 @@ def creat_app():
 
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))  
+
     return app
+
 
 def create_database(app):
     with app.app_context():
