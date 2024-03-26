d14fc35d1f88ed2037cdb8de7bfa6a3b5c1c5c2a
Bartłomiej Różańśki <rozanski.bartlomiej@gmail.com>
2024-03-26 05:50:35 +0100

    fixed calculator.html problem

--- website/calculator.py
+++ website/calculator.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 import json
 import Controller
 
 calculator_bp = Blueprint('calculator', __name__)
 
 @calculator_bp.route('/calculator', methods=['GET', 'POST'])
+@login_required
 def calculator():
     if request.method == 'POST':
         form = request.form
@@ -13,5 +15,5 @@ def calculator():
         product = Controller.calculate_price_for_web(json_data)
         print(vars(product))
 
-    return render_template("calculator.html")
+    return render_template("calculator.html", user=current_user)
 
